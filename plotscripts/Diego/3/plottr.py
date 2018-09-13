5a221bf081b2f28ceae59d4cc6e2b569b47d0664
jmcourt <J.M.Court@soton.ac.uk>
2018-09-13 18:33:56 +0100

    i quit soton im home now

---
+++ plotscripts/Diego/3/plottr.py
@@ -0,0 +1,87 @@
+from matplotlib import pyplot as pl
+from matplotlib import gridspec as gs
+import numpy as np
+
+grid=gs.GridSpec(3,1)
+
+pl.figure(figsize=(7,8))
+
+ax1=pl.subplot(grid[0])
+ax2=pl.subplot(grid[1])
+ax3=pl.subplot(grid[2])
+
+igr_x=[]
+igr_y=[]
+igr_e=[]
+
+grs_x=[]
+grs_y=[]
+grs_e=[]
+
+bp__x=[]
+bp__y=[]
+bp__e=[]
+
+a=open('GRS.dat')
+for line in a:
+   l=line.split()
+   grs_x.append(float(l[0]))
+   grs_y.append(float(l[1]))
+   grs_e.append(float(l[2]))
+a.close()
+
+a=open('IGR.dat')
+for line in a:
+   l=line.split()
+   igr_x.append(float(l[0]))
+   igr_y.append(float(l[1]))
+   igr_e.append(float(l[2]))
+a.close()
+
+a=open('BP.dat')
+for line in a:
+   l=line.split()
+   bp__x.append(float(l[0]))
+   bp__y.append(float(l[2]))
+   bp__e.append(float(l[3]))
+a.close()
+
+grs_x=np.array(grs_x)
+igr_x=np.array(igr_x)
+bp__x=np.array(bp__x)
+
+grs_s=5
+grs_f=300#150
+grs_m=0000
+grs_M=8000
+ax1.plot(grs_x-grs_s,grs_y,'k')
+ax1.errorbar(grs_x-grs_s,grs_y,yerr=grs_e,fmt='0.7',zorder=-900)
+ax1.set_xlim(0,grs_f-grs_s)
+ax1.set_ylim(grs_m,grs_M)
+ax1.text((grs_f-grs_s)/11.,(grs_M-grs_m)*(8/10.)+grs_m,'GRS 1915+105')
+ax1.set_ylabel('Rate (cts/s/PCU)')
+
+igr_s=383
+igr_f=530#470
+igr_m=0
+igr_M=600
+ax2.plot(igr_x-igr_s,igr_y,'k')
+ax2.errorbar(igr_x-igr_s,igr_y,yerr=igr_e,fmt='0.7',zorder=-900)
+ax2.set_xlim(0,igr_f-igr_s)
+ax2.set_ylim(igr_m,igr_M)
+ax2.text((igr_f-igr_s)/11.,(igr_M-igr_m)*(8/10.)+igr_m,'IGR J17091-3624')
+ax2.set_ylabel('Rate (cts/s/PCU)')
+
+bp__s=30
+bp__f=1030#1000
+bp__m=50
+bp__M=250
+ax3.plot(bp__x-bp__s,bp__y,'k')
+ax3.errorbar(bp__x-bp__s,bp__y,yerr=bp__e,fmt='0.7',zorder=-900)
+ax3.set_xlim(0,bp__f-bp__s)
+ax3.set_ylim(bp__m,bp__M)
+ax3.text((bp__f-bp__s)/11.,(bp__M-bp__m)*(8/10.)+bp__m,'GRO J1744-28')
+ax3.set_xlabel('Time (s)')
+ax3.set_ylabel('Rate (cts/s/PCU)')
+
+pl.show()
